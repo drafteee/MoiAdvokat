e042722bc08570c161d5f16e715fa66b199ee71d
Kozlovski Sergei <sergej_-96@mail.ru>
2021-08-27 18:07:25 +0300

    up

--- client/src/containers/Orders/index.js
+++ client/src/containers/Orders/index.js
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
-import { Table, Tag, Space } from 'antd';
+import { Table, Tag, Space, Modal, Button, Form, Input, InputNumber } from 'antd';
 import { orderActions } from "./store/actions";
+
 import 'antd/lib/table/style/index.css'
 import 'antd/lib/dropdown/style/index.css'
 import 'antd/lib/radio/style/index.css'
@@ -9,6 +10,20 @@ import 'antd/lib/tooltip/style/index.css'
 import 'antd/lib/tag/style/index.css'
 import 'antd/lib/pagination/style/index.css'
 import 'antd/lib/notification/style/index.css'
+import 'antd/lib/modal/style/index.css'
+import 'antd/lib/form/style/index.css'
+import 'antd/lib/input-number/style/index.css'
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const Orders = () => {
 
 const columns = [
   {
@@ -34,13 +49,17 @@ const columns = [
       <Space size="middle">
         <a>Invite {record.FirstName}</a>
         <a>Delete</a>
+        <Button type="primary" onClick={showModal}>
+          Отозваться
+      </Button>
       </Space>
     ),
   },
 ];
 
 
-const Orders = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const dispatch = useDispatch();
   const orderList =  useSelector(state => state.orderReducer.orders)
   const getAll = useCallback(
@@ -53,8 +72,78 @@ const Orders = () => {
     getAll()
   }, [])
 
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
+    <>
     <Table columns={columns} dataSource={orderList} />
+    
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Form {...layout} name="nest-messages" onFinish={onFinish}>
+        <Form.Item
+          name={['user', 'name']}
+          label="Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={['user', 'email']}
+          label="Email"
+          rules={[
+            {
+              type: 'email',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={['user', 'age']}
+          label="Age"
+          rules={[
+            {
+              type: 'number',
+              min: 0,
+              max: 99,
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item name={['user', 'website']} label="Website">
+          <Input />
+        </Form.Item>
+        <Form.Item name={['user', 'introduction']} label="Introduction">
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      </Modal>
+    </>
   )
 }
 
---
+++ src/LawyerService.Entities/Order/OrderResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerService.Entities.Order
+{
+    /// <summary>
+    /// Адвокаты, которые отозвались
+    /// </summary>
+    public class OrderResponse : BaseEntity
+    {
+        public long OrderId { get; set; }
+        public long LawyerId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime[] Dates { get; set; }
+    }
+}
